ae57e39923a57ded2e093eae4b4e0387b263d2e7
Krishna Tirole <krishnatirole05@gmail.com>
2026-02-11 22:18:07 +0530

    18_AsyncPatter4.js

---
+++ NodeFile/18_AsyncPatter4.js
@@ -0,0 +1,20 @@
+const { readFile, writeFile } = require('fs');
+const util = require('util');
+
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+
+const start = async() => {
+    try{
+        const first = await  readFilePromise('./content/first.txt', 'utf8');
+        const second = await readFilePromise('./content/second.txt', 'utf8');
+        await writeFilePromise('./content/AsynPattern.txt', `This is awesom : ${first} ${second}`)
+        console.log(first, second)
+       
+    } catch(err) {
+        console.log(err)
+    }
+    
+}
+start()
